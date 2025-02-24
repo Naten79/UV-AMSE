f45997c8e59dba756d9c85b8236a9f08fcf02b01
nathan <nathandelaunay2004@gmail.com>
2025-02-24 15:17:52 +0100

    .

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -10,12 +10,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Tak1")),
-        body: Center(
-          child: Image.network(
-            'https://picsum.photos/512/512',
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.network(
+                'https://picsum.photos/512/512',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SliderExample(),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class SliderExample extends StatefulWidget {
+  const SliderExample({super.key});
+
+  @override
+  State<SliderExample> createState() => _SliderExampleState();
+}
+
+class _SliderExampleState extends State<SliderExample> {
+  double _currentSliderPrimaryValue = 0.2;
+  double _currentSliderSecondaryValue = 0.5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Slider(
+          value: _currentSliderPrimaryValue,
+          secondaryTrackValue: _currentSliderSecondaryValue,
+          label: _currentSliderPrimaryValue.round().toString(),
+          onChanged: (double value) {
+            setState(() {
+              _currentSliderPrimaryValue = value;
+            });
+          },
+        ),
+        Slider(
+          value: _currentSliderSecondaryValue,
+          label: _currentSliderSecondaryValue.round().toString(),
+          onChanged: (double value) {
+            setState(() {
+              _currentSliderSecondaryValue = value;
+            });
+          },
+        ),
+      ],
     );
   }
 }
