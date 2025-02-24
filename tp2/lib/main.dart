6a150b88aad449b62062f3ac7043b4781cb033ba
nathan <nathandelaunay2004@gmail.com>
2025-02-24 17:14:13 +0100

    .

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -22,6 +22,10 @@ class MyApp extends StatelessWidget {
               padding: const EdgeInsets.all(16.0),
               child: SliderExample(),
             ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TileWidget(),
+            ),
           ],
         ),
       ),
@@ -64,6 +68,51 @@ class _SliderExampleState extends State<SliderExample> {
           },
         ),
       ],
+    );
+  }
+}
+
+class Tile {
+  String imageURL;
+  Alignment alignment;
+
+  Tile({required this.imageURL, required this.alignment});
+
+  Widget croppedImageTile() {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 0.3,
+            heightFactor: 0.3,
+            child: Image.network(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TileWidget extends StatelessWidget {
+  final Tile tile = Tile(
+      imageURL: 'https://picsum.photos/512', alignment: Alignment(0, 0));
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150.0,
+      height: 150.0,
+      child: Container(
+        margin: EdgeInsets.all(20.0),
+        child: InkWell(
+          child: tile.croppedImageTile(),
+          onTap: () {
+            print("Tapped on tile");
+          },
+        ),
+      ),
     );
   }
 }
